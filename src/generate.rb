71794da636ae290b9508801f980197101533bc81
Jon Marshall <jon.marshall@betterup.co>
2022-10-27 11:46:28 -0400

    Trying to get pull requests to work for private repos

--- src/generate.rb
+++ src/generate.rb
@@ -1,11 +1,21 @@
 #!/usr/bin/env ruby
 
+match, owner, repo = ARGV.first&.match(/([\w_-]+)\/([\w_-]+)/).to_a
+raise 'Expected exactly one argument of the format [GitHub owner]/[GitHub repo]' unless match
+
 require 'bundler/inline'
 
 gemfile do
   source "https://rubygems.org"
   gem "octokit", "~> 5.0"
   gem "magic_cloud"
+  gem 'pry'
+  gem 'pry-byebug'
 end
 
 puts 'Dependencies installed'
+
+client = Octokit::Client.new(access_token: ENV['GH_ACCESS_TOKEN'])
+binding.pry
+client.pull_requests(match)
+
